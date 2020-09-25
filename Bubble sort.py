ca677e644facb7f0ea5df62d7cce2b05e645ebe9
Ars187 <71893695+Ars187@users.noreply.github.com>
2020-09-25 22:09:29 +0530

    Update Bubble sort.py

--- Bubble sort.py
+++ Bubble sort.py
@@ -1,10 +1,10 @@
 #Bubble Sort
 
-lst=eval(input('Enter list'))
+lst=eval(input('Enter list')) #Reading unsorted list from user
 n=len(lst)
-for i in range(n):
+for i in range(n): 
     for j in range(n-(i+1)):
-        if lst[j]>lst[j+1]:
+        if lst[j]>lst[j+1]: #Checking if the number greater than the number succeeding it
             lst[j],lst[j+1]=lst[j+1],lst[j]
 print(lst)
     
